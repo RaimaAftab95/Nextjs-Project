9602672496b5f7dd1cc6909a706825d73b068339
RaimaAftab95 <raima.aftab2022@gmail.com>
2025-04-05 19:42:01 +0500

    setup Next.js app
    setup Next.js app with Acernity UI for component styling

---
+++ music-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+     <main>
+      <h1 className="mt-2 text-2xl font-bold text-red-950 text-center">Welcome to My Next.js App</h1>
+    </main>
+  );
+}
